0f37ea0615c5930cc51cad9bf3bd10ba54438832
Quang Phuc Nguyen <quangphuc789@gmail.com>
2015-11-11 17:08:41 +0000

    leftover app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var app = angular.module('fakefruit', []);
-app.controller('login', function($scope) {
+app.controller('login', function($scope, $http) {
     $scope.usr = "Username";
     $scope.pwd = "Password";
     $scope.lgn = "Login";
+    
+    $http.get("http://www.w3schools.com/angular/customers.php")
+    .success(function(response) {
+        $scope.names = response.records;
+        console.log($scope.names);
+    });
 });
