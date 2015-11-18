a8ef9655223d02be23152534e064582fdaed7cdc
tdkhanhvu <tdkhanhvu@gmail.com>
2015-11-19 00:18:47 +0800

    formatting quiz

--- js/QuizController.js
+++ js/QuizController.js
@@ -21,6 +21,19 @@
             $scope.submitAnswer('');
         };
 
+        $scope.nextQuestion = function(){
+            if ($scope.timer != null)
+                $timeout.cancel($scope.timer);
+
+            $scope.answer = '';
+            $scope.answerId = null;
+            $scope.question = $scope.questions.splice(randomIntFromInterval(0,
+                $scope.questions.length - 1), 1)[0];
+            console.log($scope.question.details);
+            $scope.percentComplete = Math.floor((1 - $scope.questionLeft * 1.0 / $scope.totalQuestion) * 100);
+            $scope.questionLeft--;
+        };
+
         $scope.submitAnswer = function(id){
 //            $scope.countdown -= 1;
             $scope.submitId = id;
@@ -45,15 +58,7 @@
             }
 
             if ($scope.questions.length != 0)
-                $timeout(function(){
-                    $scope.answer = '';
-                    $scope.answerId = null;
-                    $scope.question = $scope.questions.splice(randomIntFromInterval(0,
-                            $scope.questions.length - 1), 1)[0];
-                    console.log($scope.question.details);
-                    $scope.percentComplete = Math.floor((1 - $scope.questionLeft * 1.0 / $scope.totalQuestion) * 100);
-                    $scope.questionLeft--;
-                },timeOut);
+                $scope.timer = $timeout($scope.nextQuestion,timeOut);
 
 
 //            if ($scope.countdown < 1){
