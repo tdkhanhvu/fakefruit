6eebb2df0e5a875ba7a7e11eed70678617beed44
tdkhanhvu <tdkhanhvu@gmail.com>
2015-12-02 03:05:22 +0800

    fix url not updated for Facebook

--- js/angulike.js
+++ js/angulike.js
@@ -33,17 +33,17 @@
                           console.log('renderLinkButton called ');
                           if (!!attrs.fbLike && !scope.fbLike && !watchAdded) {
                               // wait for data if it hasn't loaded yet
-                              watchAdded = true;
-                              var unbindWatch = scope.$watch('fbLike', function (newValue, oldValue) {
-                                  if (newValue) {
-                                      renderLikeButton();
-                                      
-                                      // only need to run once
-                                    //   unbindWatch();
-                                  }
-                                  
-                              });
-                              return;
+//                              watchAdded = true;
+//                              var unbindWatch = scope.$watch('fbLike', function (newValue, oldValue) {
+//                                  if (newValue) {
+//                                      renderLikeButton();
+//
+//                                      // only need to run once
+//                                    //   unbindWatch();
+//                                  }
+//
+//                              });
+//                              return;
                           } else {
                               element.html('<div class="fb-like"' + (!!scope.fbLike ? ' data-href="' + scope.fbLike + '"' : '') + ' data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>');
                               $window.FB.XFBML.parse(element.parent()[0]);
