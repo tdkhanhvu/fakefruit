31aed29c95f380ff6c42c1279bfa45f91663a814
tdkhanhvu <tdkhanhvu@gmail.com>
2015-12-02 22:23:05 +0800

    fix url not updated for Facebook

--- js/angulike.js
+++ js/angulike.js
@@ -31,24 +31,24 @@
                       var watchAdded = false;
                       function renderLikeButton() {
                           console.log('renderLinkButton called ');
-                          if (!!attrs.fbLike && !scope.fbLike && !watchAdded) {
-                              // wait for data if it hasn't loaded yet
-                              watchAdded = true;
-                              var unbindWatch = scope.$watch('fbLike', function (newValue, oldValue) {
-                                  console.log('new value:' + newValue + '!');
-                                  if (newValue) {
-                                      renderLikeButton();
-
-                                      // only need to run once
-                                    //   unbindWatch();
-                                  }
-
-                              });
-                              return;
-                          } else {
+//                          if (!!attrs.fbLike && !scope.fbLike && !watchAdded) {
+//                              // wait for data if it hasn't loaded yet
+//                              watchAdded = true;
+//                              var unbindWatch = scope.$watch('fbLike', function (newValue, oldValue) {
+//                                  console.log('new value:' + newValue + '!');
+//                                  if (newValue) {
+//                                      renderLikeButton();
+//
+//                                      // only need to run once
+//                                    //   unbindWatch();
+//                                  }
+//
+//                              });
+//                              return;
+//                          } else {
                               element.html('<div class="fb-like"' + (!!scope.fbLike ? ' data-href="' + scope.fbLike + '"' : '') + ' data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>');
                               $window.FB.XFBML.parse(element.parent()[0]);
-                          }
+//                          }
                       }
                   }
               };
