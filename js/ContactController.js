114c5d09c3c00d161d11f59574bf981fd58c21d3
tdkhanhvu <tdkhanhvu@gmail.com>
2015-12-08 02:26:41 +0800

    contact form

--- js/ContactController.js
+++ js/ContactController.js
@@ -7,9 +7,19 @@
             Url: FruitService.getCurrentUrl()
         };
 
+        $scope.name = '';
+        $scope.email = '';
+        $scope.position = '';
+        $scope.comment = '';
+        $scope.submitted = false;
+
         $scope.getImage = function(imageName) {
             var path = FruitService.getStaticImage(imageName);
             return path;
+        }
+
+        $scope.submitForm = function() {
+            $scope.submitted = true;
         }
     };
 
